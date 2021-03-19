bb92c9d947011282ea231202af7d255dc2f4d5c5
K-lil001 <70827555+K-lil001@users.noreply.github.com>
2021-03-19 10:58:00 -0500

    asd
    asd

---
+++ EstDatos1_Lab1_v2/src/estdatos1_lab1_v2/EstDatos1_Lab1_v2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estdatos1_lab1_v2;
+
+/**
+ *
+ * @author khali
+ */
+public class EstDatos1_Lab1_v2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frameI ui = new frameI();
+        ui.setVisible(true);
+    }
+    
+}
